8cae21d1ac42cacc3924fe8e9bf6453622ae6d56
Hari Soni <harisoni535@gmail.com>
2026-01-08 13:23:05 +0000

    base update

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/scrollstory-showcase/" : "/",
+  base: mode === "production" ? "/SmrithiSite/" : "/",
   server: {
     host: "::",
     port: 8080,
